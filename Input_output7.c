c6f23fc5550cdb2544ba0a0f33e9024d44c86e3c
Kavindu1997 <56603291+Kavindu1997@users.noreply.github.com>
2020-12-14 11:17:03 +0530

    Input & output

---
+++ Input_output7.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	int x,y;
+	char text[20];
+	scanf("%2d %d %*f %5s",&x,&y,text);
+	printf("%d %d %s",x,y,text);
+    
+}
---
+++ Input_output8.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	printf("Pi=%8.5f\n",3.14159);
+	printf("Pi=%-8.5f\n",3.14159);
+	printf("There are %d %s in the tree",22,"apples\n");
+	printf("%%");
+	
+}
