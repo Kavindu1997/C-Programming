76e221906d3ee7a813104002aa24f9ad1accedea
Kavindu1997 <56603291+Kavindu1997@users.noreply.github.com>
2020-12-20 00:22:18 +0530

    q_sort

---
+++ fpointers5.c
@@ -0,0 +1,24 @@
+/*Quick Sort*/
+/*void qsort(void *base, size_t num, size_t width, int (*compare)(const void *, const void *)) */
+#include<stdio.h>
+#include<stdlib.h>
+int compare(const void*,const void*);
+
+int main(){
+	int arr[5]={52,23,56,19,4};
+	int num,width,i;
+	
+	num=sizeof(arr)/sizeof(arr[0]);
+	width=sizeof(arr[0]);
+	qsort((void*)arr,num,width,compare);
+	
+}
+
+int compare(const void *ele1,const void *ele2){
+	if((*(int*)ele1) == (*(int*)ele2))
+	  return 0;
+	else if((*(int*)ele1) < (*(int*)ele2)) 
+	  return -1;
+	else
+	  return 1;   
+}
